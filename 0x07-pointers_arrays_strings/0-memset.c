33fc58e46df702674c002eaaabc3d9d8a297e9b4
NIYICODE <olaniyieluwole1@gmail.com>
2023-01-02 23:24:09 -0800

    updates

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,21 +10,12 @@
 char *_memset(char *s, char b, unsigned int n)
 
 {
-
-        int i = 0;
-
-
-
-        for (; n > 0; i++)
+        int i;
+        for (i = 0; i < n; i++)
 
         {
-	
-	                s[i] = b;
-	
-	                n--;
-	
+	 	 s[i] = b;
+		 
 	}
-
-        return s;
-
+        return (s);
 }
